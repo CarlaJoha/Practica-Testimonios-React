45bca1064d2f4a5782628b03af61e11aed12fd69
carlajoha <carlajoha.work@gmail.com>
2023-04-24 16:10:26 -0400

    estilos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import "./App.css";
 import Testimonio from "./components/Testimonio/Testimonio.jsx";
 import { data } from "./data";
+import logo from "./images/logo5.png";
 
 function App() {
   // console.log(data);
   return (
     <div className="App">
-      <div className="contenerdor-principal">
+      <div className="contenedor-principal">
+        <div className="contenedor-cabecera">
+          <img className="logo" src={logo} alt="logo" />
+        </div>
         <h1>Sección de Testimonios con React:</h1>
+        <div >
+          <div className="card">
+          {data.map((testimonio, index) => {
+            return (
+              <Testimonio
+                key={index}
+                nombre={testimonio.nombre}
+                pais={testimonio.pais}
+                imagen={testimonio.imagen}
+                cargo={testimonio.cargo}
+                empresa={testimonio.empresa}
+                testimonio={testimonio.testimonio}
+              />
+            );
+          })}
+          </div>
+        </div>
       </div>
-      {data.map((testimonio, index) => {
-        return (
-          <Testimonio
-            key={index}
-            nombre={testimonio.nombre}
-            pais={testimonio.pais}
-            imagen={testimonio.imagen}
-            cargo={testimonio.cargo}
-            empresa={testimonio.empresa}
-            testimonio={testimonio.testimonio}
-          />
-        );
-      })}
     </div>
   );
 }
--- src/components/Testimonio/Testimonio.jsx
+++ src/components/Testimonio/Testimonio.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import "./Testimonio.css"
+import "./Testimonio.css";
 
-const Testimonio = ({imagen, nombre, cargo, pais, testimonio, empresa}) => {
- 
+const Testimonio = ({ imagen, nombre, cargo, pais, testimonio, empresa }) => {
   return (
     <div className="contenedor-testimonio">
-      <img
-        className="imagen-testimonio"
-        src={require(`../../images/user-${imagen}.jpg`)}
-        alt="img user"
-      />
-      <div className="contenerdor-texto-testimonio">
-        <p className="nombre-testimonio"><strong>{nombre}</strong> en {pais}</p>
-        <p className="cargo-testimonio">{cargo} en <strong>{empresa}</strong></p>
+      <div className="contenedor-imagen">
+        <img
+          // className="imagen-testimonio"
+          src={require(`../../images/user-${imagen}.jpg`)}
+          alt="img user"
+          className="imagen"
+        />
+      </div>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{nombre}</strong> en {pais}
+        </p>
+        <p className="cargo-testimonio">
+          {cargo} en <strong>{empresa}</strong>
+        </p>
         <p className="texto-testimonio">"{testimonio}"</p>
       </div>
     </div>
